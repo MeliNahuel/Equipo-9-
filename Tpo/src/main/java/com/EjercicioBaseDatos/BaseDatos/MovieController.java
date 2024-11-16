97dedc72340ed508bf563342d628ce1fded606a5
TobiasOM <tobiass2505@gmail.com>
2024-11-15 22:13:15 -0300

    Update MovieController.java

--- Tpo/src/main/java/com/EjercicioBaseDatos/BaseDatos/MovieController.java
+++ Tpo/src/main/java/com/EjercicioBaseDatos/BaseDatos/MovieController.java
@@ -26,4 +26,35 @@ public class MovieController {
     Flux<MovieEntity> getMovies() {
         return movieRepository.findAll();
     }
+@GetMapping("/grafo")
+    public Mono<String> getGrafo() {
+        return movieRepository.findAll()
+                .collectList() // Convertimos el Flux a un Mono<List<MovieEntity>>
+                .map(movies -> {
+                    Grafo grafo = new Grafo(movies); // Construimos el grafo con la lista de películas
+
+                    return grafo.toString(); // Devolvemos la representación en String del grafo
+                });
+    }
+
+    @GetMapping("/bfs")
+    public Mono<String> getBFS() {
+        return movieRepository.findAll()
+                .collectList() // Convertimos el Flux a un Mono<List<MovieEntity>>
+                .map(movies -> {
+                    Grafo grafo = new Grafo(movies); // Construimos el grafo con la lista de películas grafo
+
+                    return grafo.BFS(movies.get(0)); // Devolvemos la representación en String del
+                });
+    }
+    @GetMapping("/dfs")
+    public Mono<String> getDFS() {
+        return movieRepository.findAll()
+                .collectList() // Convertimos el Flux a un Mono<List<MovieEntity>>
+                .map(movies -> {
+                    Grafo grafo = new Grafo(movies); // Construimos el grafo con la lista de películas grafo
+
+                    return grafo.DFS(movies.get(0)); // Devolvemos la representación en String del
+                });
+    }
 }
